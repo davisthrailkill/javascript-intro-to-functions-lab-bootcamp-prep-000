840fd0cdc3ec35945aded819fc9e1393230cc833
Davis Thrailkill <davis.thrailkill@gmail.com>
2018-01-14 22:14:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ function whisper(string){
 }
 
 function logShout(string){
-  console.log(string.toUpperCase);
+  console.log(string.toUpperCase());
 }
 
-function 
+function logWhisper(string){
+  console.log(string.toLowerCase());
+}
